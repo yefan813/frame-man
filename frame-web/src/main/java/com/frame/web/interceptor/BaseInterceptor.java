8e5ab690e1777996b0860603ce57964826dbd82c
yefan <394818428@qq.com>
2016-07-09 01:48:46 +0800

    asd

--- frame-web/src/main/java/com/frame/web/interceptor/BaseInterceptor.java
+++ frame-web/src/main/java/com/frame/web/interceptor/BaseInterceptor.java
@@ -1,7 +1,6 @@
 package com.frame.web.interceptor;
 
 import java.io.IOException;
-import java.security.GeneralSecurityException;
 import java.security.MessageDigest;
 import java.util.Date;
 import java.util.HashMap;
@@ -124,13 +123,15 @@ public class BaseInterceptor implements HandlerInterceptor {
 		basestring.append(secret);
 
 		// 使用MD5对待签名串求签
-		byte[] bytes = null;
-		try {
-			MessageDigest md5 = MessageDigest.getInstance("MD5");
-			bytes = md5.digest(basestring.toString().getBytes("UTF-8"));
-		} catch (GeneralSecurityException ex) {
-			throw new IOException(ex);
-		}
+		String sign = MD5Encode(basestring.toString(), "UTF-8").toLowerCase();
+		
+//		byte[] bytes = null;
+//		try {
+//			MessageDigest md5 = MessageDigest.getInstance("MD5");
+//			bytes = md5.digest(basestring.toString().getBytes("UTF-8"));
+//		} catch (GeneralSecurityException ex) {
+//			throw new IOException(ex);
+//		}
 
 		// 将MD5输出的二进制结果转换为小写的十六进制
 
@@ -141,7 +142,7 @@ public class BaseInterceptor implements HandlerInterceptor {
 		// }
 		// sign.append(hex);
 		// }
-		return byteArrayToHexString(bytes);
+		return sign;
 	}
 
 	private static String byteArrayToHexString(byte b[]) {
@@ -159,6 +160,20 @@ public class BaseInterceptor implements HandlerInterceptor {
 		int d1 = n / 16;
 		int d2 = n % 16;
 		return hexDigits[d1] + hexDigits[d2];
+	}
+
+	public static String MD5Encode(String origin, String charsetname) {
+		String resultString = null;
+		try {
+			resultString = new String(origin);
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			if (charsetname == null || "".equals(charsetname))
+				resultString = byteArrayToHexString(md.digest(resultString.getBytes()));
+			else
+				resultString = byteArrayToHexString(md.digest(resultString.getBytes(charsetname)));
+		} catch (Exception exception) {
+		}
+		return resultString;
 	}
 
 	private static final String hexDigits[] = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d",
