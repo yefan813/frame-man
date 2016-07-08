69e74f31f6760869cc782033137a5049df6c9a9f
yefan <394818428@qq.com>
2016-07-09 01:37:43 +0800

    asd

--- frame-web/src/main/java/com/frame/web/interceptor/BaseInterceptor.java
+++ frame-web/src/main/java/com/frame/web/interceptor/BaseInterceptor.java
@@ -29,15 +29,16 @@ import com.frame.service.AppSecretService;
 
 /**
  * 基础操作拦截器.
+ * 
  * @date 2015/12/03
  */
-public class BaseInterceptor implements HandlerInterceptor  {
+public class BaseInterceptor implements HandlerInterceptor {
 	private static final Logger logger = LoggerFactory.getLogger(BaseInterceptor.class);
-	private String key;	// 与passport系统交换公钥
+	private String key; // 与passport系统交换公钥
 
 	@Resource
 	private AppSecretService appSecretService;
-	
+
 	public void setKey(String key) {
 		this.key = key;
 	}
@@ -48,45 +49,44 @@ public class BaseInterceptor implements HandlerInterceptor  {
 		String apiKey = request.getParameter("apiKey");
 		String timestamp = request.getParameter("timestamp");
 		String sign = request.getParameter("sign");
-		
-		if(StringUtils.isEmpty(apiKey) || StringUtils.isEmpty(timestamp) || StringUtils.isEmpty(sign) ){
-			logger.error("接口过滤器调用，传入的参数错误，传入的参数为apiKey:【{}】，timestamp【{}】，sign：【{}】",apiKey,timestamp,sign);
+
+		if (StringUtils.isEmpty(apiKey) || StringUtils.isEmpty(timestamp) || StringUtils.isEmpty(sign)) {
+			logger.error("接口过滤器调用，传入的参数错误，传入的参数为apiKey:【{}】，timestamp【{}】，sign：【{}】", apiKey, timestamp, sign);
 			return false;
 		}
 		Date now = new Date();
-		long appDate =  Long.valueOf(timestamp);
+		long appDate = Long.valueOf(timestamp);
 		long afteDate = appDate + 60 * 1000;
 		Date appRequestTime = new Date(afteDate);
-		
-		if(now.after(appRequestTime)){//超过60s 请求无效
-			logger.error("请求超过60s，请求无效,请求来自apiKey：【{}】",apiKey);
+
+		if (now.after(appRequestTime)) {// 超过60s 请求无效
+			logger.error("请求超过60s，请求无效,请求来自apiKey：【{}】", apiKey);
 			return false;
 		}
-		
+
 		AppSecret appSecret = new AppSecret();
 		appSecret.setApiKey(apiKey);
 		appSecret.setYn(YnEnum.Normal.getKey());
 		List<AppSecret> resList = appSecretService.selectEntryList(appSecret);
-		if(CollectionUtils.isEmpty(resList)){
+		if (CollectionUtils.isEmpty(resList)) {
 			logger.error("没找到相关的apikey");
 			return false;
-		}else{
+		} else {
 			logger.error("找到相关的apikey,验证参数是否被篡改");
 			appSecret = resList.get(0);
 		}
-		
-		// 对参数名进行字典排序  
+
+		// 对参数名进行字典排序
 		Map paramMap = new HashMap(request.getParameterMap());
 		paramMap.remove("sign");
-		Map<String,String> resMap = transToMAP(paramMap);
-		String codes = getSignature(resMap,appSecret.getSecretKey());  
-		
-		
-		if(sign.equals(codes)){
+		Map<String, String> resMap = transToMAP(paramMap);
+		String codes = getSignature(resMap, appSecret.getSecretKey());
+
+		if (sign.equals(codes)) {
 			logger.error("参数验证成功！");
 			return true;
-		}else{
-			logger.error("参数验证失败！服务器传入的sign：【{}】， 服务器的sign：【{}】",sign,codes);
+		} else {
+			logger.error("参数验证失败！服务器传入的sign：【{}】， 服务器的sign：【{}】", sign, codes);
 			return false;
 		}
 	}
@@ -101,71 +101,93 @@ public class BaseInterceptor implements HandlerInterceptor  {
 			throws Exception {
 	}
 
-	
 	/**
 	 * 签名生成算法
-	 * @param HashMap<String,String> params 请求参数集，所有参数必须已转换为字符串类型
-	 * @param String secret 签名密钥
+	 * 
+	 * @param HashMap<String,String>
+	 *            params 请求参数集，所有参数必须已转换为字符串类型
+	 * @param String
+	 *            secret 签名密钥
 	 * @return 签名
 	 * @throws IOException
 	 */
-	public static String getSignature(Map<String,String> params, String secret) throws IOException{
-	    // 先将参数以其参数名的字典序升序进行排序
-	    Map<String, String> sortedParams = new TreeMap<String, String>(params);
-	    Set<Entry<String, String>> entrys = sortedParams.entrySet();
-	 
-	    // 遍历排序后的字典，将所有参数按"key=value"格式拼接在一起
-	    StringBuilder basestring = new StringBuilder();
-	    for (Entry<String, String> param : entrys) {
-	        basestring.append(param.getKey()).append("=").append(param.getValue());
-	    }
-	    basestring.append(secret);
-	 
-	    // 使用MD5对待签名串求签
-	    byte[] bytes = null;
-	    try {
-	        MessageDigest md5 = MessageDigest.getInstance("MD5");
-	        bytes = md5.digest(basestring.toString().getBytes("UTF-8"));
-	    } catch (GeneralSecurityException ex) {
-	        throw new IOException(ex);
-	    }
-	 
-	    // 将MD5输出的二进制结果转换为小写的十六进制
-	    StringBuilder sign = new StringBuilder();
-	    for (int i = 0; i < bytes.length; i++) {
-	        String hex = Integer.toHexString(bytes[i] & 0xFF);
-	        if (hex.length() == 1) {
-	            sign.append("0");
-	        }
-	        sign.append(hex);
-	    }
-	    return sign.toString();
+	public static String getSignature(Map<String, String> params, String secret) throws IOException {
+		// 先将参数以其参数名的字典序升序进行排序
+		Map<String, String> sortedParams = new TreeMap<String, String>(params);
+		Set<Entry<String, String>> entrys = sortedParams.entrySet();
+
+		// 遍历排序后的字典，将所有参数按"key=value"格式拼接在一起
+		StringBuilder basestring = new StringBuilder();
+		for (Entry<String, String> param : entrys) {
+			basestring.append(param.getKey()).append("=").append(param.getValue());
+		}
+		basestring.append(secret);
+
+		// 使用MD5对待签名串求签
+		byte[] bytes = null;
+		try {
+			MessageDigest md5 = MessageDigest.getInstance("MD5");
+			bytes = md5.digest(basestring.toString().getBytes("UTF-8"));
+		} catch (GeneralSecurityException ex) {
+			throw new IOException(ex);
+		}
+
+		// 将MD5输出的二进制结果转换为小写的十六进制
+
+		// for (int i = 0; i < bytes.length; i++) {
+		// String hex = Integer.toHexString(bytes[i] & 0xFF);
+		// if (hex.length() == 1) {
+		// sign.append("0");
+		// }
+		// sign.append(hex);
+		// }
+		return byteArrayToHexString(bytes);
 	}
-	
-	 private static Map<String,String> transToMAP(Map parameterMap){
-	      // 返回值Map
-	      Map<String,String> returnMap = new HashMap<String,String> ();
-	      Iterator entries = parameterMap.entrySet().iterator();
-	      Map.Entry entry;
-	      String name = "";
-	      String value = "";
-	      while (entries.hasNext()) {
-	          entry = (Map.Entry) entries.next();
-	          name = (String) entry.getKey();
-	          Object valueObj = entry.getValue();
-	          if(null == valueObj){
-	              value = "";
-	          }else if(valueObj instanceof String[]){
-	              String[] values = (String[])valueObj;
-	              for(int i=0;i<values.length;i++){
-	                  value = values[i] + ",";
-	              }
-	              value = value.substring(0, value.length()-1);
-	          }else{
-	              value = valueObj.toString();
-	          }
-	          returnMap.put(name, value);
-	      }
-	      return  returnMap;
-	  }
+
+	private static String byteArrayToHexString(byte b[]) {
+		StringBuffer resultSb = new StringBuffer();
+		for (int i = 0; i < b.length; i++)
+			resultSb.append(byteToHexString(b[i]));
+
+		return resultSb.toString();
+	}
+
+	private static String byteToHexString(byte b) {
+		int n = b;
+		if (n < 0)
+			n += 256;
+		int d1 = n / 16;
+		int d2 = n % 16;
+		return hexDigits[d1] + hexDigits[d2];
+	}
+
+	private static final String hexDigits[] = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d",
+			"e", "f" };
+
+	private static Map<String, String> transToMAP(Map parameterMap) {
+		// 返回值Map
+		Map<String, String> returnMap = new HashMap<String, String>();
+		Iterator entries = parameterMap.entrySet().iterator();
+		Map.Entry entry;
+		String name = "";
+		String value = "";
+		while (entries.hasNext()) {
+			entry = (Map.Entry) entries.next();
+			name = (String) entry.getKey();
+			Object valueObj = entry.getValue();
+			if (null == valueObj) {
+				value = "";
+			} else if (valueObj instanceof String[]) {
+				String[] values = (String[]) valueObj;
+				for (int i = 0; i < values.length; i++) {
+					value = values[i] + ",";
+				}
+				value = value.substring(0, value.length() - 1);
+			} else {
+				value = valueObj.toString();
+			}
+			returnMap.put(name, value);
+		}
+		return returnMap;
+	}
 }
